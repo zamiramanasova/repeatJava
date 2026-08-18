09dd4ece1dea77e3892c8c93b077f580881cbbb9
Zamira <keldibaevaz@gmail.com>
2026-08-18 22:02:27 +0300

    first

---
+++ src/Lesson10/A.java
@@ -0,0 +1,12 @@
+package Lesson10;
+
+import Lesson9.Student;
+
+public class A {
+    public static void main(String[] args) {
+        Student st2 = new Student();
+        System.out.println(st2.z);
+        System.out.println(Student.c);
+
+    }
+}
---
+++ src/Lesson10/Homework/A.java
@@ -0,0 +1,6 @@
+package Lesson10.Homework;
+
+public class A {
+    public int one = 10;
+    public int two = 2;
+}
