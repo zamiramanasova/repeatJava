b21ee3f75739f5ab3999b233f443dd52c170dd67
Zamira <keldibaevaz@gmail.com>
2026-08-19 20:46:06 +0300

    first

---
+++ src/Lesson4/BankAccount.java
@@ -0,0 +1,14 @@
+package Lesson4;
+
+public class BankAccount {
+    int id=1;
+
+    public static void main(String[] args) {
+
+        BankAccount MyAccount = new BankAccount();
+        BankAccount YourAccount = MyAccount;
+
+        System.out.println(MyAccount.id);
+        System.out.println(YourAccount); // ссылка
+    }
+}
