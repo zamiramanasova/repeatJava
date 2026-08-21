97d3fe1a7c8b49b3249e5b36c1d851fe7ebeb05b
Zamira <keldibaevaz@gmail.com>
2026-08-21 22:08:43 +0300

    first

---
+++ src/Lesson10/Homework/p2/p3/p4/D.java
@@ -0,0 +1,21 @@
+package Lesson10.Homework.p2.p3.p4;
+
+import Lesson10.Homework.A;
+import Lesson10.Homework.p2.B;
+import Lesson10.Homework.p2.p3.C;
+import Lesson10.Homework.p2.p3.p4.p5.E;
+import static Lesson10.Homework.p2.B.*;
+
+public class D {
+    public static void main(String[] args) {
+        A a1 = new A();
+        C c1 = new C();
+        E e1 = new E();
+        System.out.println(a1.one + " " + a1.two);
+        System.out.println(c1.c1 + " " + c1.c2);
+        System.out.println(e1.e1 + " " + e1.e2);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
