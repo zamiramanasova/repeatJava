1d8419b6be544640f390af573ae2b5a72c352918
Zamira <keldibaevaz@gmail.com>
2026-08-21 22:05:39 +0300

    first

---
+++ src/Lesson12/Car.java
@@ -0,0 +1,28 @@
+package Lesson12;
+
+public class Car {
+    int engine;
+    int doorCount;
+
+    public Car(int engine, int doorCount) {
+        this.engine = engine;
+        this.doorCount = doorCount;
+    }
+}
+
+class CarTest {
+    public static void main(String[] args) {
+        Car c1 = new Car(3, 4);
+        Car c2 = new Car(2,5);
+
+        if (c1.engine > c2.engine) {
+            if (c1.doorCount > c2.doorCount) {
+                System.out.println("Мощность мотора и еоличество дверей у первой машины больше");
+            } else {
+                System.out.println("Мощность мотора у первой машины больше, а количество дверей меньше");
+            }
+        } else {
+            System.out.println("Мощность мотора у первой машины меньше");
+        }
+    }
+}
