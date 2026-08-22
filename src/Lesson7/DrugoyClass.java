31c3674edb199ddb54264d32c994f0396d7d00e7
Zamira <keldibaevaz@gmail.com>
2026-08-22 22:17:09 +0300

    first

---
+++ src/Lesson7/DrugoyClass.java
@@ -0,0 +1,9 @@
+package Lesson7;
+
+public class DrugoyClass {
+    public static void main(String[] args) {
+        Employee em = new Employee(500);
+        System.out.println(em.salary);
+        em.dvoynayaZP();
+    }
+}
