b07e68395c949647d34f43ca5496bd30409ee129
Zamira <keldibaevaz@gmail.com>
2026-08-24 22:22:13 +0300

    first

---
+++ src/Lesson7_2/Employee.java
@@ -0,0 +1,23 @@
+package Lesson7_2;
+
+public class Employee {
+    public static void main(String[] args) {
+        Lesson7.homework.Employee emp = new Lesson7.homework.Employee(1);
+//        System.out.println(emp.id);
+//        Lesson7.homework.Employee emp2 = new Lesson7.homework.Employee("Petrov");
+//       System.out.println(emp2.surname);
+//        Lesson7.homework.Employee emp3 = new Lesson7.homework.Employee(340000.00);
+//        System.out.println(emp3.salary);
+        emp.method1();
+        emp.method2();
+        emp.method3();
+
+//        emp2.method1();
+//        emp2.method2();
+//        emp2.method3();
+//
+//        emp3.method1();
+//        emp3.method2();
+//        emp3.method3();
+    }
+}
