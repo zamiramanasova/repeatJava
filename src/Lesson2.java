81b214e00ffaa1a48cdadc49096b095c1ecce277
Zamira <keldibaevaz@gmail.com>
2026-08-27 22:56:29 +0300

    lambda

---
+++ src/Lesson2.java
@@ -0,0 +1,37 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+        byte b3 = 100;
+
+        short s1 = 5;
+        short s2 = -10;
+        short s3 = 0;
+
+        int i1 = 500;
+
+        float f1 = 3.14f;
+        float f2 = 2.5f;
+
+        double d1 = 5.5;
+        double d2 = 87.65;
+
+        char c1 = 'A';
+        char c2 = 'a';
+
+        char c5 = 500; // 10
+        char c6 = '\u0500'; //16
+
+        boolean bul1 = true;
+        boolean bul2 = false;
+
+        int a1 = 60;
+
+        int a2 = 0b111100;
+
+        System.out.println(c5);
+
+
+    }
+}
