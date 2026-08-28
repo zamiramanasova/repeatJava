b700bd395bcc7a843adda32e653447c730480018
Zamira <keldibaevaz@gmail.com>
2026-08-28 22:57:54 +0300

    lambda

---
+++ src/Lesson9/homework/Main.java
@@ -0,0 +1,16 @@
+package Lesson9.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        Car c2 = new Car();
+        Car c3;
+        Car c4 = c1;
+        Car c5 = c2;
+        c3 = c4;
+        Car c6 = c5;
+    }
+}
+class Car {
+
+}
