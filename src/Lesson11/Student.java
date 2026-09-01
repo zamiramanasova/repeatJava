6cf223c0690bea73332fd9103bc0bbdf0b8b8ea7
Zamira <keldibaevaz@gmail.com>
2026-09-01 15:24:09 +0300

    lambda

---
+++ src/Lesson11/Student.java
@@ -0,0 +1,24 @@
+package Lesson11;
+
+public class Student {
+    public String name;
+    public int course;
+    public double grade;
+
+    public Student(String name, int course, double grade) {
+        this.name = name;
+        this.course = course;
+        this.grade = grade;
+    }
+
+    public static void changeName(Student s1) {
+        s1.name = "Vasiliy";
+    }
+
+    public static void main(String[] args) {
+        Student st1 = new Student("Ivan", 3, 9.5);
+        Student st2 = new Student("Petr", 1, 5.3);
+        changeName(st1);
+        System.out.println(st1.name);
+    }
+}
