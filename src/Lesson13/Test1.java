f277bd2b4932ef4a21cc2fcba2065304a36a06da
Zamira <keldibaevaz@gmail.com>
2026-09-03 09:15:18 +0300

    lambda

---
+++ src/Lesson13/Test1.java
@@ -0,0 +1,33 @@
+package Lesson13;
+
+public class Test1 {
+}
+
+class Student {
+    int grade;
+
+    Student(int grade) {
+        this.grade = grade;
+    }
+
+    public static void main(String[] args) {
+        Student st1 = new Student(12);
+
+        switch(st1.grade) {
+            case 2:
+                System.out.println(2);
+                break;
+            case 3:
+                System.out.println(3);
+                break;
+            case 4:
+                System.out.println(4);
+                break;
+            case 5:
+                System.out.println(5);
+                break;
+            default:
+                System.out.println("Wrong");
+        }
+    }
+}
