7287e70132b3fe39c17affcd24d5d1cb7b3e396f
Zamira <keldibaevaz@gmail.com>
2026-09-04 09:27:37 +0300

    lambda

---
+++ src/Lesson16/Test1.java
@@ -0,0 +1,14 @@
+package Lesson16;
+
+public class Test1 {
+    public static void main(String[] args) {
+        String s10 = new String("Privet");
+        String s2 = "Privet";
+        System.out.println("Privet");
+        System.out.println("poka");
+        System.out.println("poka" == "privet");
+        String a1 = new String("Privet");
+
+
+    }
+}
