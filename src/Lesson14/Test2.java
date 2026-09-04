9324a6150d2ea12570f290e7ed8834c9ae30abb7
Zamira <keldibaevaz@gmail.com>
2026-09-04 09:30:19 +0300

    lambda

---
+++ src/Lesson14/Test2.java
@@ -0,0 +1,15 @@
+package Lesson14;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int a = 5;
+
+        if (a < 10) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}
