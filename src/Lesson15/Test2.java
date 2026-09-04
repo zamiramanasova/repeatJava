65f816f84e14c2bb9818d2df83013e8f0102d3e6
Zamira <keldibaevaz@gmail.com>
2026-09-04 09:30:32 +0300

    lambda

---
+++ src/Lesson15/Test2.java
@@ -0,0 +1,14 @@
+package Lesson15;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int money = 100;
+
+        while (money > 0) {
+            System.out.println("Delayte stavky");
+            System.out.println("Vy proigraly");
+            money = money - 10;
+            System.out.println(money);
+        }
+    }
+}
