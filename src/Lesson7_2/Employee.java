ab9fafc40b74509a16cda2824f3c7ea3c6fde734
Zamira <keldibaevaz@gmail.com>
2026-09-08 22:03:14 +0300

    lambda

--- src/Lesson7_2/Employee.java
+++ src/Lesson7_2/Employee.java
@@ -5,7 +5,7 @@ public class Employee {
         Lesson7.homework.Employee emp = new Lesson7.homework.Employee(1);
 //      System.out.println(emp.id);
 //      Lesson7.homework.Employee emp2 = new Lesson7.homework.Employee("Petrov");
-//       System.out.println(emp2.surname);
+//      System.out.println(emp2.surname);
 //        Lesson7.homework.Employee emp3 = new Lesson7.homework.Employee(340000.00);
 //        System.out.println(emp3.salary);
         emp.method1();
