493cffeb5b4ac51d6e98120e0788df0e118459ca
Zamira <keldibaevaz@gmail.com>
2026-09-08 21:59:10 +0300

    lambda

--- src/Lesson7/homework/EmployeeTest.java
+++ src/Lesson7/homework/EmployeeTest.java
@@ -6,7 +6,7 @@ public class EmployeeTest {
             System.out.println(emp.id);
             Employee emp2 = new Employee("Petrov");
             System.out.println(emp2.surname + emp2.age);
-//           Employee emp3 = new Employee(340000.00);
+//          Employee emp3 = new Employee(340000.00);
 //            System.out.println(emp3.salary);
             emp.method1();
             emp.method2();
