f791a70efb69e7632b8b4283f684f8ee40da7297
Zamira <keldibaevaz@gmail.com>
2026-09-08 22:01:44 +0300

    lambda

--- src/Lesson7_2/Employee.java
+++ src/Lesson7_2/Employee.java
@@ -3,7 +3,7 @@ package Lesson7_2;
 public class Employee {
     public static void main(String[] args) {
         Lesson7.homework.Employee emp = new Lesson7.homework.Employee(1);
-//        System.out.println(emp.id);
+//       System.out.println(emp.id);
 //        Lesson7.homework.Employee emp2 = new Lesson7.homework.Employee("Petrov");
 //       System.out.println(emp2.surname);
 //        Lesson7.homework.Employee emp3 = new Lesson7.homework.Employee(340000.00);
