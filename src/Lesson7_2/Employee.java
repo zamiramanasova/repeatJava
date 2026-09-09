1adb58c7f09b005034c27f78075861ca6e6cc1fb
Zamira <keldibaevaz@gmail.com>
2026-09-09 22:19:50 +0300

    lambda

--- src/Lesson7_2/Employee.java
+++ src/Lesson7_2/Employee.java
@@ -14,7 +14,7 @@ public class Employee {
 
 //      emp2.method1();
 //      emp2.method2();
-//        emp2.method3();
+//       emp2.method3();
 //
 //        emp3.method1();
 //        emp3.method2();
