0ebd76248dacae8114855a300195894855faf739
Zamira <keldibaevaz@gmail.com>
2026-09-12 17:18:24 +0300

    lambda

--- src/Lesson17/Test.java
+++ src/Lesson17/Test.java
@@ -3,7 +3,7 @@ package Lesson17;
 public class Test {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("123");
-        StringBuilder sb2 = sb.append("45");// не создается объект, а будет ссылыаться на sb
+        StringBuilder sb2 = sb.append("45");// не создается объект, а будет ссылаться на sb
         // и результат у них будет одинаковый
 
         sb2 = sb2.append("6").append("7");
