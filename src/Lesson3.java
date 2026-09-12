90907d700fbf43a1f35d21086c2d0825e85fb3db
Zamira <keldibaevaz@gmail.com>
2026-09-12 17:15:06 +0300

    lambda

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -11,7 +11,7 @@ public class Lesson3 {
 //      int a = 5;
 //      int b = 3;
 //      int c = a - ++b;
-//       System.out.println(z);
+//      System.out.println(z);
 //       System.out.println(b);
 
         double d = 1.3;
