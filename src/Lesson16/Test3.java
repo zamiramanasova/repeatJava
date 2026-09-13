80ef0fdd075ae79fcf6b9881ffed45b1faf1fc60
Zamira <keldibaevaz@gmail.com>
2026-09-13 19:19:48 +0300

    lambda

--- src/Lesson16/Test3.java
+++ src/Lesson16/Test3.java
@@ -4,7 +4,7 @@ public class Test3 {
     public static void main(String[] args) {
         String s1 = new String("ok");
         String s2 = new String("ok");
-        System.out.println(s2 == s1);// false
+        System.out.println(s2 == s1); // false
         System.out.println(s1.equals(s2));// true
         String s3 = "privet";
         String s4 = "privet";
