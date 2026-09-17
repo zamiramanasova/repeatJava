2dafaf111bf816f48b788388e546514d47e3cd1c
Zamira <keldibaevaz@gmail.com>
2026-09-17 21:37:06 +0300

    lambda

--- src/Lesson6/homework/Student.java
+++ src/Lesson6/homework/Student.java
@@ -35,7 +35,7 @@ class StudentMain{
         Student s2 = new Student(1, "Zaur ","Zaurovich ", "IT ");
         System.out.println(s2.id + " " + s2.name + " " + s2.surname + " " + s2.course);
         Student s3 = new Student(2, "Bomba ", "Bombovich ", "IT",1998,5.0,5.0,4.0);
-        System.out.println(s3.id + "  " + s3.name + "  " + s3.surname + " " + s3.course + " " + s3.year + " " + s3.numberMath + " " +
+        System.out.println(s3.id + "  " + s3.name + "  " + s3.surname + "  " + s3.course + " " + s3.year + " " + s3.numberMath + " " +
                 s3.numberEconomic + "  " + s3.numberEnglish);
     }
 }
