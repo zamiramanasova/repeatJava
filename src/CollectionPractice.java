e0a463213b6196ca6a20ea8f72eaa1063d080568
Zamira <keldibaevaz@gmail.com>
2026-09-18 15:50:37 +0300

    lambda

--- src/CollectionPractice.java
+++ src/CollectionPractice.java
@@ -1,3 +1,23 @@
-public class CollectionPractice {
+import practiceLambda.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionPractice {
+    public static void main(String[] args) {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(1, "a"));
+        userList.add(new User(2, "b"));
+        userList.add(new User(3, "c"));
+        userList.add(new User(4, "d"));
+        userList.add(new User(5, "f"));
+        userList.add(new User(6, "g"));
+        userList.add(new User(7, "h"));
+        userList.add(new User(8, "aj"));
+        userList.add(new User(9, "ak"));
+        userList.add(new User(10, "al"));
+        userList.add(new User(11, "az"));
+
+
+    }
 }
---
+++ src/practiceLambda/User.java
@@ -0,0 +1,27 @@
+package practiceLambda;
+
+public class User {
+    int age;
+    String name;
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
