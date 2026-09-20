f486d5b9a9f01c397f1cf35c0888ff49292a7d65
Zamira <keldibaevaz@gmail.com>
2026-09-20 16:29:55 +0300

    lambda

--- src/Lesson4/StudentTest.java
+++ src/Lesson4/StudentTest.java
@@ -5,7 +5,7 @@ public class StudentTest {
 
         Student student1 = new Student(1,"Mark","Markov",
                 1989,4.0,3.5,5.0);
-        Student student2 = new Student(2,"Bova","Bovych",
+        Student student2 = new Student(2,"Vova","Bovych",
                 1989,5.0,4.5,4.0);
         Student student3 = new Student(3,"Berk","Berkov",
                 1989,3.0,4.0,4.5);
