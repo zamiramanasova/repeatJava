9736743f4609c4c7e98cc128a7c4ca3755a830c8
Zamira <keldibaevaz@gmail.com>
2026-09-20 16:30:24 +0300

    lambda

--- src/Lesson4/StudentTest.java
+++ src/Lesson4/StudentTest.java
@@ -3,7 +3,7 @@ package Lesson4;
 public class StudentTest {
     public static void main(String[] args) {
 
-        Student student1 = new Student(1,"Mark","Markov",
+        Student student1 = new Student(1,"Mark","Markov ",
                 1989,4.0,3.5,5.0);
         Student student2 = new Student(2,"Vova","Bovych",
                 1989,5.0,4.5,4.0);
