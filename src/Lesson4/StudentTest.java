bed80cc78a23bb3e4db5ce24ba5f5c6480f082df
Zamira <keldibaevaz@gmail.com>
2026-09-20 16:31:10 +0300

    lambda

--- src/Lesson4/StudentTest.java
+++ src/Lesson4/StudentTest.java
@@ -7,7 +7,7 @@ public class StudentTest {
                 1989,4.0,3.5,5.0);
         Student student2 = new Student(2,"Vova","Bovych ",
                 1989,5.0,4.5,4.0);
-        Student student3 = new Student(3,"Berk","Berkov",
+        Student student3 = new Student(3,"Berk","Berkov ",
                 1989,3.0,4.0,4.5);
 
         student1.count();
